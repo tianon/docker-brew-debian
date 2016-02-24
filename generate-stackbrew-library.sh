b4fc26d682094e8237382d1dff0d4b11d1c55989
Tianon Gravi <admwiggin@gmail.com>
2016-02-23 19:45:06 -0800

    Support multiple branches for generate-stackbrew-library.sh (so that unstable and testing can be updated without ever touching stable suites)

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -25,25 +25,32 @@ cat <<-'EOH'
 # maintainer: Paul Tagliamonte <paultag@debian.org> (@paultag)
 EOH
 
-commitRange='master..dist'
-commitCount="$(git rev-list "$commitRange" --count 2>/dev/null || true)"
-if [ "$commitCount" ] && [ "$commitCount" -gt 0 ]; then
-	echo
-	echo '# commits:' "($commitRange)"
-	git log --format=format:'- %h %s%n%w(0,2,2)%b' "$commitRange" | sed 's/^/#  /'
-fi
+branches=( master dist-stable dist-unstable )
+
+for branch in "${branches[@]}"; do
+	if [ "$branch" = 'master' ]; then
+		continue
+	fi
+	commitRange="master..$branch"
+	commitCount="$(git rev-list "$commitRange" --count 2>/dev/null || true)"
+	if [ "$commitCount" ] && [ "$commitCount" -gt 0 ]; then
+		echo
+		echo '# commits:' "($commitRange)"
+		git log --format=format:'- %h %s%n%w(0,2,2)%b' "$commitRange" | sed 's/^/#  /'
+	fi
+done
 
 for version in "${versions[@]}"; do
-	commit="$(git log -1 --format='format:%H' -- "$version")"
+	commit="$(git log -1 --format='format:%H' "${branches[@]}" -- "$version")"
 	versionAliases=()
 	if [ -z "${noVersion[$version]}" ]; then
 		tarball="$version/rootfs.tar.xz"
-		fullVersion="$(tar -xvf "$tarball" etc/debian_version --to-stdout 2>/dev/null)"
+		fullVersion="$(git show "$commit:$tarball" | tar -xvJ etc/debian_version --to-stdout 2>/dev/null || true)"
 		if [ -z "$fullVersion" ] || [[ "$fullVersion" == */sid ]]; then
-			fullVersion="$(eval "$(tar -xvf "$tarball" etc/os-release --to-stdout 2>/dev/null)" && echo "$VERSION" | cut -d' ' -f1)"
+			fullVersion="$(eval "$(git show "$commit:$tarball" | tar -xvJ etc/os-release --to-stdout 2>/dev/null || true)" && echo "$VERSION" | cut -d' ' -f1)"
 			if [ -z "$fullVersion" ]; then
 				# lucid...
-				fullVersion="$(eval "$(tar -xvf "$tarball" etc/lsb-release --to-stdout 2>/dev/null)" && echo "$DISTRIB_DESCRIPTION" | cut -d' ' -f2)" # DISTRIB_DESCRIPTION="Ubuntu 10.04.4 LTS"
+				fullVersion="$(eval "$(git show "$commit:$tarball" | tar -xvJ etc/lsb-release --to-stdout 2>/dev/null || true)" && echo "$DISTRIB_DESCRIPTION" | cut -d' ' -f2)" # DISTRIB_DESCRIPTION="Ubuntu 10.04.4 LTS"
 			fi
 		else
 			while [ "${fullVersion%.*}" != "$fullVersion" ]; do
@@ -55,14 +62,14 @@ for version in "${versions[@]}"; do
 			versionAliases+=( $fullVersion )
 		fi
 	fi
-	versionAliases+=( $version $(cat "$version/suite" 2>/dev/null || true) ${aliases[$version]} )
+	versionAliases+=( $version $(git show "$commit:$version/suite" 2>/dev/null || true) ${aliases[$version]} )
 	
 	echo
 	for va in "${versionAliases[@]}"; do
 		echo "$va: ${url}@${commit} $version"
 	done
 	
-	if [ -s "$version/backports/Dockerfile" ]; then
+	if [ "$(git show "$commit:$version/backports/Dockerfile" 2>/dev/null || true)" ]; then
 		echo
 		echo "$version-backports: ${url}@${commit} $version/backports"
 	fi
